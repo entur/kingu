863fe0420044b0a8b2f68af0f31e033ab05fe586
Assad Riaz <assad.riaz@entur.org>
2022-05-31 13:45:51 +0200

    Refactor QuayListConverter

--- src/main/java/org/entur/kingu/netex/mapping/converter/QuayListConverter.java
+++ src/main/java/org/entur/kingu/netex/mapping/converter/QuayListConverter.java
@@ -18,14 +18,15 @@ package org.entur.kingu.netex.mapping.converter;
 import ma.glasnost.orika.MappingContext;
 import ma.glasnost.orika.converter.BidirectionalConverter;
 import ma.glasnost.orika.metadata.Type;
-import org.rutebanken.netex.model.Quays_RelStructure;
 import org.entur.kingu.model.Quay;
+import org.rutebanken.netex.model.Quays_RelStructure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class QuayListConverter extends BidirectionalConverter<Set<Quay>, Quays_RelStructure> {
@@ -40,7 +41,7 @@ public class QuayListConverter extends BidirectionalConverter<Set<Quay>, Quays_R
 
         Quays_RelStructure quays_relStructure = new Quays_RelStructure();
 
-        logger.debug("Mapping {} quays to netex", quays != null ? quays.size() : 0);
+        logger.debug("Mapping {} quays to netex", quays.size());
 
         quays.forEach(quay -> {
             org.rutebanken.netex.model.Quay netexQuay = mapperFacade.map(quay, org.rutebanken.netex.model.Quay.class);
@@ -53,15 +54,12 @@ public class QuayListConverter extends BidirectionalConverter<Set<Quay>, Quays_R
     public Set<Quay> convertFrom(Quays_RelStructure quays_relStructure, Type<Set<Quay>> type, MappingContext mappingContext) {
         logger.debug("Mapping {} quays to internal model", quays_relStructure != null ? quays_relStructure.getQuayRefOrQuay().size() : 0);
         Set<Quay> quays = new HashSet<>();
-        if(quays_relStructure.getQuayRefOrQuay() != null) {
-            quays_relStructure.getQuayRefOrQuay().stream()
+        if(quays_relStructure != null && quays_relStructure.getQuayRefOrQuay() != null) {
+            quays = quays_relStructure.getQuayRefOrQuay().stream()
                     .filter(object -> object instanceof org.rutebanken.netex.model.Quay)
                     .map(object -> ((org.rutebanken.netex.model.Quay) object))
-                    .map(netexQuay -> {
-                        Quay tiamatQuay = mapperFacade.map(netexQuay, Quay.class);
-                        return tiamatQuay;
-                    })
-                    .forEach(quay -> quays.add(quay));
+                    .map(netexQuay -> mapperFacade.map(netexQuay, Quay.class))
+                    .collect(Collectors.toSet());
         }
         
         return quays;
